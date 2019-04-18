380ba078b43aa380827f7de2940cd497ba9f05ec
Van-Dang Nguyen <vdnguyen@kth.se>
2019-04-18 20:46:15 +0200

    Add files via upload

--- GetPartitionMarkers.py
+++ GetPartitionMarkers.py
@@ -1,61 +1,62 @@
 # Copyright Van-Dang NGUYEN 2019
 import os, sys
 
-mshfile=sys.argv[1]
+def GetPartitionMarkers(mshfile):
 
-fext=mshfile[len(mshfile)-3:len(mshfile)]
-if not(fext =="msh"):
-  print("Sorry!!! The program supports .msh only! Please check the input file format...");
-  exit(0);
+  fext=mshfile[len(mshfile)-3:len(mshfile)]
+  if not(fext =="msh"):
+    print("Sorry!!! The program supports .msh only! Please check the input file format...");
+    exit(0);
 
-print("Extracting cell markers from: "+mshfile+" ...");
+  print("Extracting cell markers from: "+mshfile+" ...");
 
-filewithoutextention = os.path.splitext(os.path.basename(mshfile))[0];
+  filewithoutextention = os.path.splitext(os.path.basename(mshfile))[0];
 
-f = open(mshfile, "r")
-lineList = f.readlines()
-f.close()
+  f = open(mshfile, "r")
+  lineList = f.readlines()
+  f.close()
 
-lastline = lineList[len(lineList)-2].split(" ")
-last_id = int(lastline[0])
+  lastline = lineList[len(lineList)-2].split(" ")
+  last_id = int(lastline[0])
 
-first_id = -100;
+  first_id = -100;
 
-for x in lineList:
-  x = x.split(" ")
-  if len(x) == len(lastline):
+  for x in lineList:
+    x = x.split(" ")
+    if len(x) == len(lastline):
         first_id = int(x[0])
         break;
 
-if len(lastline)==8:
+  if len(lastline)==8:
         dim = 2;
-elif len(lastline)==9:
+  elif len(lastline)==9:
         dim = 3;
-else:
+  else:
         print("Invalid dimension. Please double check msh2xml!");
 
-numcells = last_id - first_id + 1
+  numcells = last_id - first_id + 1
 
-outfile = 'volmk_'+filewithoutextention+'.xml'
-filename = open(outfile,'w');
-filename.write('<?xml version="1.0"?>\n')
-filename.write('<dolfin xmlns:dolfin="http://fenicsproject.org">\n')
-filename.write('  <mesh_function>\n')
-filename.write('    <mesh_value_collection type="uint" dim="'+str(dim)+'" size="'+str(numcells)+'">\n');
+  outfile = 'volmk_'+filewithoutextention+'.xml'
+  filename = open(outfile,'w');
+  filename.write('<?xml version="1.0"?>\n')
+  filename.write('<dolfin xmlns:dolfin="http://fenicsproject.org">\n')
+  filename.write('  <mesh_function>\n')
+  filename.write('    <mesh_value_collection type="uint" dim="'+str(dim)+'" size="'+str(numcells)+'">\n');
 
-cmpts=[];
-for x in lineList:
-  x = x.split(" ")
-  if len(x) == len(lastline):
+  cmpts=[];
+  for x in lineList:
+    x = x.split(" ")
+    if len(x) == len(lastline):
 	  filename.write('      <value cell_index="'+str(int(x[0])-first_id)+'" local_entity="0" value="'+x[4]+'" />\n');
 	  if (len(cmpts)==0 or not(x[4]==cmpts[len(cmpts)-1])):
                 cmpts.append(x[4])
 
-filename.write('    </mesh_value_collection>\n');
-filename.write('  </mesh_function>\n');
-filename.write('</dolfin>\n');
-filename.close();
+  filename.write('    </mesh_value_collection>\n');
+  filename.write('  </mesh_function>\n');
+  filename.write('</dolfin>\n');
+  filename.close();
 
-print("Extracted successfully on: "+str(numcells)+" elements")
-print("Partition marker list: " + str(cmpts))
-print("Wrote to: "+outfile)
+  print("Extracted successfully on: "+str(numcells)+" elements")
+  print("Partition marker list: " + str(cmpts))
+  print("Wrote to: "+outfile)
+  return cmpts
