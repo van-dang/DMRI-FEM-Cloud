3a80fd26bb3d0c3098932b8b81b9b1b3894b3c6e
Van-Dang Nguyen <vdnguyen@kth.se>
2019-04-18 21:05:02 +0200

    Update GetPartitionMarkers.py

--- GetPartitionMarkers.py
+++ GetPartitionMarkers.py
@@ -1,7 +1,7 @@
 # Copyright Van-Dang NGUYEN 2019
 import os, sys
 
-def GetPartitionMarkers(mshfile):
+def GetPartitionMarkers(mshfile, pfile=''):
 
   fext=mshfile[len(mshfile)-3:len(mshfile)]
   if not(fext =="msh"):
@@ -36,7 +36,10 @@ def GetPartitionMarkers(mshfile):
 
   numcells = last_id - first_id + 1
 
-  outfile = 'volmk_'+filewithoutextention+'.xml'
+  if (pfile==''):
+     outfile = 'volmk_'+filewithoutextention+'.xml'
+  else:
+     outfile = pfile
   filename = open(outfile,'w');
   filename.write('<?xml version="1.0"?>\n')
   filename.write('<dolfin xmlns:dolfin="http://fenicsproject.org">\n')
