0ed9794e902e20fad8303faba671c8e639d13f85
Van-Dang Nguyen <vdnguyen@kth.se>
2019-05-03 16:05:28 +0200

    Update DmriFemBuitInFunc.py

--- DmriFemBuitInFunc.py
+++ DmriFemBuitInFunc.py
@@ -759,7 +759,7 @@ def Post_processing(mydomain, mri_para, mri_simu, ms=''):
         print('Signal on each compartment')
         print('Sum initial0: %.3e, Signal0: %.3e'%(initial0, signal0))
         print('Sum initial1: %.3e, Signal1: %.3e'%(initial1, signal1))
-        out_text = 'b: %.3f, Signal: %.3e, Normalized signal: %.3e, kappa: %.3e, dt: %.3f, hmin: %.3f, whole_vol: %.3f, vol_of_interest: %.3f, Free signal: %.3e, elasped time %.3f (s)\n'%(mri_para.bvalue, signal, signal/voi, mydomain.kappa, mri_simu.k, mydomain.hmin, whole_vol, voi, exp(-mri_para.bvalue*mydomain.D0), mri_simu.elapsed_time)
+        out_text = 'b: %.3f, q: %.3f, Signal: %.3e, Normalized signal: %.3e, kappa: %.3e, dt: %.3f, hmin: %.3f, whole_vol: %.3f, vol_of_interest: %.3f, Free signal: %.3e, elasped time %.3f (s)\n'%(mri_para.bvalue, mri_para.qvalue, signal, signal/voi, mydomain.kappa, mri_simu.k, mydomain.hmin, whole_vol, voi, exp(-mri_para.bvalue*mydomain.D0), mri_simu.elapsed_time)
         
         V0 = FunctionSpace(mydomain.mesh0, mydomain.Ve);
         V1 = FunctionSpace(mydomain.mesh1, mydomain.Ve);
@@ -775,7 +775,7 @@ def Post_processing(mydomain, mri_para, mri_simu, ms=''):
     else:
         ur, ui = split(mri_simu.u_0)
         signal = assemble(ur*dx);
-        out_text = 'b: %.3f, Signal: %.3e, Normalized signal: %.3e, dt: %.3f, hmin: %.3e, whole_vol: %.3f, vol_of_interest: %.3f, Free signal: %.3e, elasped time %.3f (s)\n'%(mri_para.bvalue, signal, signal/voi, mri_simu.k, mydomain.hmin, whole_vol, voi, exp(-mri_para.bvalue*mydomain.D0), mri_simu.elapsed_time)
+        out_text = 'b: %.3f, q: %.3f, Signal: %.3e, Normalized signal: %.3e, dt: %.3f, hmin: %.3e, whole_vol: %.3f, vol_of_interest: %.3f, Free signal: %.3e, elasped time %.3f (s)\n'%(mri_para.bvalue, mri_para.qvalue, signal, signal/voi, mri_simu.k, mydomain.hmin, whole_vol, voi, exp(-mri_para.bvalue*mydomain.D0), mri_simu.elapsed_time)
         V = FunctionSpace(mydomain.mymesh,mydomain.Ve);
         ur_p = project(ur,V)
         if mydomain.tdim==mydomain.gdim: 
