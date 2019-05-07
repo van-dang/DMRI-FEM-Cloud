2b4c87fd1d24dab2ec87ebf45b0e124609b5f846
Van-Dang Nguyen <vdnguyen@kth.se>
2019-05-07 18:51:27 +0200

    Add files via upload

---
+++ GCloudDmriSolver.py
@@ -0,0 +1,217 @@
+# -*- coding: utf-8 -*-
+# This program solves the Bloch-Torrey equation applied to computational diffusion MRI using 
+# the finite element method coupled with the theta-method for the spatial discretization.
+
+# The scope of usage: 
+# (1) Single domains, Multilayered structures, manifolds
+# (2) Membrane permeability for internal interfaces
+#     Artificial permeability at the external interfaces
+# (3) pure homogeneous Neumann BCs, (4) pseudo-periodic BCs
+
+# Copyright (C) 2019 Van-Dang Nguyen (vdnguyen@kth.se)
+
+# This file is part of DOLFIN.
+
+# DOLFIN is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# DOLFIN is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+
+# You should have received a copy of the GNU Lesser General Public License
+# along with DOLFIN. If not, see <http://www.gnu.org/licenses/>.
+
+# First added:  2017-10-10
+# Last changed: 2019-04-25
+
+# This demo is maintained by Van-Dang Nguyen
+# Please report possible problems to vdnguyen@kth.se
+
+# Disable warnings
+import logging
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger("rothemain.rothe_utils")
+logging.getLogger('UFL').setLevel(logging.WARNING)
+logging.getLogger('FFC').setLevel(logging.WARNING)
+
+
+import warnings
+warnings.filterwarnings("ignore")
+
+import os, sys, shutil
+
+from dolfin import *
+parameters['ghost_mode'] = 'shared_facet' 
+parameters['allow_extrapolation'] = True
+
+## default parameters
+g0, g1, g2 = 0, 1, 0; kcoeff = 3e-3; porder = 1; Nsteps = 100; bvalue = 1000; kappa = 1e-5;
+delta, Delta = 10600, 43100; T2 = 1e16; k = 200; nskip = 5; is_input_dt = 0; is_input_b = 0; is_input_q = 0;
+is_kcoeff_from_file = 1; is_T2_from_file = 1; is_IC_from_file = 1; IsDomainPeriodic = False; IsDomainMultiple = False
+PeriodicDir = [0, 0, 0]; phase = None; mesh0 = None; mesh1 = None; ic = None
+## end default parameters
+
+try:
+    ## Input parameters from command lines
+        for i in range(0, len(sys.argv)):
+                arg = sys.argv[i];
+                if arg=='-f':
+                        ffile = sys.argv[i+1];
+                        print('input file:', ffile)
+                if arg=='-M':
+                        IsDomainMultiple = int(sys.argv[i+1]);
+                        print('IsDomainMultiple:', IsDomainMultiple)
+                if arg=='-IsPeriodic' or arg=='-isperiodic':
+                        IsDomainPeriodic = int(sys.argv[i+1]);
+                        print('IsDomainPeriodic:', IsDomainPeriodic)
+                if arg=='-N':
+                        Nsteps = int(sys.argv[i+1]);
+                        print('Nsteps:', Nsteps)
+                if arg=='-b':
+                        is_input_b = 1;
+                        bvalue = float(sys.argv[i+1]);
+                        print('bvalue:', bvalue)
+                if arg=='-q':
+                        is_input_q = 1;
+                        qvalue = float(sys.argv[i+1]);
+                        print('qvalue:', bvalue)
+                if arg=='-p':
+                        kappa = float(sys.argv[i+1]);
+                        print('permeability:', kappa)
+                if arg=='-D':                       
+                        Delta = float(sys.argv[i+1]);
+                        print('Delta:', Delta)
+                if arg=='-d':
+                        delta = float(sys.argv[i+1]);
+                        print('delta:', delta)
+                if arg=='-K':
+                        is_kcoeff_from_file = 0
+                        print("Reading diffusion coefficient from command line")
+                        kcoeff = float(sys.argv[i+1]);
+                        print('diffusion coefficient:', kcoeff)
+                if arg=='-k':
+                        is_input_dt = 1;
+                        k = float(sys.argv[i+1]);
+                        print('time step size:', k)
+                if arg=='-T2':
+                        is_T2_from_file = 0;
+                        T2 = float(sys.argv[i+1]);
+                        print('T2: ', T2)
+                if arg=='-gdir':
+                        g0 = float(sys.argv[i+1]);
+                        g1 = float(sys.argv[i+2]);
+                        g2 = float(sys.argv[i+3]);
+                        print('(g0, g1, g2):',g0, g1, g2)
+                if arg=='-pdir':
+                        PeriodicDir[0] = int(sys.argv[i+1]);
+                        PeriodicDir[1] = int(sys.argv[i+2]);
+                        PeriodicDir[2] = int(sys.argv[i+3]);
+                        print('PeriodicDir=[', PeriodicDir[0], PeriodicDir[1], PeriodicDir[2],']')                        
+except:
+        print('Something goes wrong with the inputs!')
+
+"""#Load pre-defined functions"""
+
+exists = os.path.isfile('DmriFemBuitInFunc.py')
+isupdate = False
+if (exists==False or isupdate==True):
+    if isupdate==True:
+        os.system("rm DmriFemBuitInFunc.py")
+    print("Load pre-defined functions from GitHub")
+    os.system("wget --quiet https://raw.githubusercontent.com/van-dang/MRI-Cloud/master/DmriFemBuitInFunc.py")
+
+from DmriFemBuitInFunc import *
+
+"""# Solve the Bloch-Torrey equation"""
+
+mymesh = Mesh()
+myf = HDF5File(mymesh.mpi_comm(),ffile, 'r')
+myf.read(mymesh, 'mesh', False)
+
+V_DG = FunctionSpace(mymesh, 'DG', 0)
+dofmap_DG = V_DG.dofmap()
+
+if is_kcoeff_from_file == 1:
+        print("Reading diffusion tensor from file: ", ffile)
+        d00 = Function(V_DG); d01 = Function(V_DG); d02 = Function(V_DG)
+        d10 = Function(V_DG); d11 = Function(V_DG); d12 = Function(V_DG)
+        d20 = Function(V_DG); d21 = Function(V_DG); d22 = Function(V_DG)
+        myf.read(d00, 'd00'); myf.read(d01, 'd01'); myf.read(d02, 'd02')
+        myf.read(d10, 'd10'); myf.read(d11, 'd11'); myf.read(d12, 'd12')
+        myf.read(d20, 'd20'); myf.read(d21, 'd21'); myf.read(d22, 'd22')
+
+if is_T2_from_file == 1:
+        print("Reading T2 from file: ", ffile)
+        T2 = Function(V_DG);
+        myf.read(T2, 'T2');
+                
+if IsDomainMultiple==1:
+        print("Reading phase function, mesh0, mesh1 from file: ", ffile)        
+        phase = Function(V_DG);
+        myf.read(phase, 'phase');       
+        mesh0 = Mesh();  mesh1 = Mesh();
+        myf.read(mesh0, 'mesh0', False)
+        myf.read(mesh1, 'mesh1', False)
+
+mri_simu = MRI_simulation()
+mri_para = MRI_parameters()
+
+#################################################################################
+#########################  Pre-defined parameters ###############################
+mri_para.bvalue = bvalue                             # bvalue
+mri_para.delta, mri_para.Delta = delta, Delta        # time sequence
+mri_para.set_gradient_dir(mymesh, g0, g1, g2)           # gradient direction
+mri_para.T = mri_para.Delta+mri_para.delta
+mri_para.fs_sym = sp.Piecewise(
+                    (  1., mri_para.s < mri_para.delta ),
+                    (  0., mri_para.s < mri_para.Delta ),
+                    ( -1., mri_para.s < mri_para.T ),
+                    (  0., True )  
+                ) 
+
+mri_para.Apply()
+mri_simu.k = k;                                      # time-step size
+mri_simu.nskip = 5;                                  # frequency to print ouputs
+
+mydomain = MyDomain(mymesh, mri_para)
+mydomain.phase = phase
+mydomain.mesh0 = mesh0
+mydomain.mesh1 = mesh1
+mydomain.PeriodicDir = PeriodicDir;            # Direction of the periodicity
+mydomain.IsDomainPeriodic = IsDomainPeriodic   # Confirm if the mesh if periodic
+mydomain.IsDomainMultiple = IsDomainMultiple   # Confirm if the mesh is multiple
+mydomain.kappa = kappa                         # Permeability
+mydomain.Apply()   # Call Apply before setting the diffusion tensor
+################################################################################
+if is_kcoeff_from_file == 1:
+        print("Impose diffusion tensor from file: ", ffile)
+        mydomain.ImposeDiffusionTensor(d00, d01, d02, d10, d11, d12, d20, d21, d22)
+else:
+        print("Impose diffusion coefficient from command line, D0=", kcoeff)
+        mydomain.D0 = kcoeff
+        mydomain.D = mydomain.D0;
+#################################################################################
+
+# linsolver = PETScLUSolver("mumps")
+linsolver = KrylovSolver("bicgstab")
+#linsolver.parameters["absolute_tolerance"] = 1e-4
+#linsolver.parameters["relative_tolerance"] = 1e-3
+#linsolver.parameters["maximum_iterations"] = 1000
+
+if is_IC_from_file == 1:
+        print("Reading initial conditions from file: ", ffile)
+        ic = Function(V_DG);
+        myf.read(ic, 'ic');
+        ic=project(ic, mydomain.V)
+
+mri_simu.solve(mydomain, mri_para, linsolver)
+
+ctext = ''
+if bvalue == 1000:
+        ctext = 'Torus R=[5, 7.5, 10], Delta=43.1ms, delta=10.6ms, D=[3 1 3]*1e-3 mm^2/s, kappa=1e-5m/s, gdir=100'
+Post_processing(mydomain, mri_para, mri_simu, None, ctext)
+
---
+++ PreprocessingMultiCompt.py
@@ -0,0 +1,134 @@
+import os, sys, shutil
+from dolfin import *
+
+comm = MPI.comm_world
+nprocs = comm.Get_size()
+if (nprocs>1):
+    print('Support serial computation only!')
+    sys.exit()
+
+
+exists = os.path.isfile('DmriFemBuitInFunc.py')
+isupdate = False
+if (exists==False or isupdate==True):
+    if isupdate==True:
+        os.system("rm DmriFemBuitInFunc.py")
+    print("Load pre-defined functions from GitHub")
+    os.system("wget --quiet https://raw.githubusercontent.com/van-dang/MRI-Cloud/master/DmriFemBuitInFunc.py")
+from DmriFemBuitInFunc import *
+
+
+"""# Working on the mesh"""
+
+geo_choice = 2
+################################################################################
+############## Create two-layered disk using mshr in FEniCS ####################
+if geo_choice == 1:
+    R1, R2 = 5, 10;
+    origin = Point(0.,0.)
+    circle = Circle(origin, R1, segments=32)
+    domain = Circle(origin, R2, segments=32)
+    domain.set_subdomain(1, circle)
+    mymesh = generate_mesh(domain, 15) # 15 is the resolution
+    cmpt_mesh = generate_mesh(circle, 15)
+    cellmarker, phase, partition_marker, partion_list = Create_phase_func(mymesh, cmpt_mesh, None)    
+
+################################################################################
+############## Create multilayered domains using gmsh ##########################
+if geo_choice == 2:
+    mesh_name = "multi_layered_disk"
+    # mesh_name = "multi_layered_cylinder"
+    # mesh_name = "multi_layered_sphere"
+    # mesh_name = "multi_layered_torus"
+    
+    is_partition_function_exist = os.path.isfile('GetPartitionMarkers.py')
+    if is_partition_function_exist==False:
+        os.system('wget --quiet https://raw.githubusercontent.com/van-dang/MRI-Cloud/mesh/GetPartitionMarkers.py')
+
+    is_geo_file_exist = os.path.isfile(mesh_name+'.geo')  
+    if is_geo_file_exist==False:
+        os.system('wget --quiet https://raw.githubusercontent.com/van-dang/MRI-Cloud/mesh/'+mesh_name+'.geo')
+
+    # Modify .geo file from 4 layers to 3 layers      
+    os.system("sed -i 's/5, 7.5, 10, 13/5, 7.5, 10/g' "+mesh_name+".geo")
+      
+    # Create mesh from geo file by gmsh
+    os.system('gmsh -3 '+mesh_name+'.geo -o '+mesh_name+'.msh')
+    
+    # Convert .msh to .xml using dolfin-convert
+    os.system('dolfin-convert '+mesh_name+'.msh '+mesh_name+'.xml')
+       
+    mymesh = Mesh(mesh_name+".xml");  
+
+    from GetPartitionMarkers import *
+
+    GetPartitionMarkers(mesh_name+".msh", "pmk_"+mesh_name+".xml")
+
+    partition_marker = MeshFunction("size_t", mymesh, mymesh.topology().dim())
+
+    File("pmk_"+mesh_name+".xml")>>partition_marker
+
+    cellmarker, phase, partition_marker, partion_list = Create_phase_func(mymesh, None, partition_marker)    
+
+################################################################################
+############## Download the existing mesh and submesh ##########################
+if geo_choice == 3:
+    is_file_exist = os.path.isfile("multi_layer_torus.xml")  
+    if is_file_exist==False:
+        os.system('wget --quiet https://raw.githubusercontent.com/van-dang/MRI-Cloud/master/comri/meshes/multi_layer_torus.xml.zip')
+        os.system('wget --quiet https://raw.githubusercontent.com/van-dang/MRI-Cloud/master/comri/meshes/multi_layer_torus_compt1.xml.zip')
+        os.system('unzip -q multi_layer_torus.xml.zip')
+        os.system('unzip -q multi_layer_torus_compt1.xml.zip')
+
+    mymesh = Mesh("multi_layer_torus.xml");  
+    cmpt_mesh = Mesh('multi_layer_torus_compt1.xml')
+    cellmarker, phase, partition_marker, partion_list = Create_phase_func(mymesh, cmpt_mesh, None)    
+    
+################################################################################
+############## Save, Plot phase functions and submeshes to verify ##############
+print("Partition markers:", partion_list)
+mesh0 = SubMesh(mymesh, cellmarker, 0)
+mesh1 = SubMesh(mymesh, cellmarker, 1)
+
+V_DG = FunctionSpace(mymesh, 'DG', 0)
+
+D0 = 3e-3
+D0_array = [D0, D0/3, D0]
+IC_array = [1, 1, 1]
+T2_array = [1e6, 1e6, 1e6]
+
+# Variable tensor
+dofmap_DG = V_DG.dofmap()
+d00 = Function(V_DG); d01 = Function(V_DG); d02 = Function(V_DG)
+d10 = Function(V_DG); d11 = Function(V_DG); d12 = Function(V_DG)
+d20 = Function(V_DG); d21 = Function(V_DG); d22 = Function(V_DG)
+T2  = Function(V_DG); disc_ic = Function(V_DG);
+        
+for cell in cells(mymesh):
+      cell_dof = dofmap_DG.cell_dofs(cell.index())
+      cmk = partition_marker[cell.index()]
+      T2.vector()[cell_dof]      = T2_array[cmk];
+      disc_ic.vector()[cell_dof] = IC_array[cmk]; 
+      d00.vector()[cell_dof]     = D0_array[cmk]; 
+      d11.vector()[cell_dof]     = D0_array[cmk]; 
+      d22.vector()[cell_dof]     = D0_array[cmk];
+
+ofile = 'files.h5';
+
+for i in range(0, len(sys.argv)):
+      arg = sys.argv[i];
+      if arg=='-o':
+            ofile = sys.argv[i+1];
+
+filename, file_extension = os.path.splitext(ofile)
+
+ofile = filename+'.h5'
+
+f = HDF5File(mymesh.mpi_comm(), ofile, 'w')
+f.write(mymesh, 'mesh'); f.write(mesh0, 'mesh0'); f.write(mesh1, 'mesh1') 
+f.write(T2, 'T2');  f.write(disc_ic, 'ic'); f.write(phase, 'phase');
+f.write(d00, 'd00'); f.write(d01, 'd01'); f.write(d02, 'd02')
+f.write(d10, 'd10'); f.write(d11, 'd11'); f.write(d12, 'd12')
+f.write(d20, 'd20'); f.write(d21, 'd21'); f.write(d22, 'd22')
+
+print("Write to ", ofile)
