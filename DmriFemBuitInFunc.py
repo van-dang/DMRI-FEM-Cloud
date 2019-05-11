4e4870dd5c5e5b0b05ab8b0313b88fe6cfb9981d
Van-Dang Nguyen <vdnguyen@kth.se>
2019-05-11 17:43:29 +0200

    Update DmriFemBuitInFunc.py

--- DmriFemBuitInFunc.py
+++ DmriFemBuitInFunc.py
@@ -500,15 +500,15 @@ class MyDomain():
         self.gdir = mri_para.gdir        
         self.gnorm = mri_para.gnorm 
         self.D0 = 3e-3
-        self.kappa_e_scalar = 3e-3
+        self.kappa_e_scalar = 3e-3/self.hmin
 
     def WeakPseudoPeridicMarker(self):        
         if self.gdim==2:
-            pmk = self.kappa_e_scalar/self.hmin*Expression("(x[0]<xmin+eps || x[0]>xmax-eps)*p0 || (x[1]<ymin+eps || x[1]>ymax-eps)*p1", 
+            pmk = self.kappa_e_scalar*Expression("(x[0]<xmin+eps || x[0]>xmax-eps)*p0 || (x[1]<ymin+eps || x[1]>ymax-eps)*p1", 
                              xmin=self.xmin, xmax=self.xmax, ymin=self.ymin, ymax=self.ymax, 
                              eps=1e-10, p0 = self.PeriodicDir[0], p1 = self.PeriodicDir[1], domain=self.mymesh, degree=1);
         if self.gdim==3:
-            pmk = self.kappa_e_scalar/self.hmin*Expression("(x[0]<xmin+eps || x[0]>xmax-eps)*p0 || (x[1]<ymin+eps || x[1]>ymax-eps)*p1 || (x[2]<zmin+eps || x[2]>zmax-eps)*p2", 
+            pmk = self.kappa_e_scalar*Expression("(x[0]<xmin+eps || x[0]>xmax-eps)*p0 || (x[1]<ymin+eps || x[1]>ymax-eps)*p1 || (x[2]<zmin+eps || x[2]>zmax-eps)*p2", 
                              xmin=self.xmin, xmax=self.xmax, ymin=self.ymin, ymax=self.ymax, zmin=self.zmin, zmax=self.zmax, 
                              eps=1e-10, p0 = self.PeriodicDir[0], p1 = self.PeriodicDir[1], p2 = self.PeriodicDir[2], domain=self.mymesh, degree=1);
         return pmk
