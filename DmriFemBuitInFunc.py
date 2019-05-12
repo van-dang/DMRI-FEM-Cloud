45fe6e235b6219a1cf7192ac4b163011efeb79f8
Van-Dang Nguyen <vdnguyen@kth.se>
2019-05-12 17:56:11 +0200

    Update DmriFemBuitInFunc.py

--- DmriFemBuitInFunc.py
+++ DmriFemBuitInFunc.py
@@ -504,13 +504,13 @@ class MyDomain():
 
     def WeakPseudoPeridicMarker(self):        
         if self.gdim==2:
-            pmk = self.kappa_e_scalar*Expression("(x[0]<xmin+eps || x[0]>xmax-eps)*p0 || (x[1]<ymin+eps || x[1]>ymax-eps)*p1", 
+            pmk = Expression("ke*((x[0]<xmin+eps || x[0]>xmax-eps)*p0 || (x[1]<ymin+eps || x[1]>ymax-eps)*p1)", 
                              xmin=self.xmin, xmax=self.xmax, ymin=self.ymin, ymax=self.ymax, 
-                             eps=1e-10, p0 = self.PeriodicDir[0], p1 = self.PeriodicDir[1], domain=self.mymesh, degree=1);
+                             eps=1e-10, p0 = self.PeriodicDir[0], p1 = self.PeriodicDir[1], ke=self.kappa_e_scalar, domain=self.mymesh, degree=1);
         if self.gdim==3:
-            pmk = self.kappa_e_scalar*Expression("(x[0]<xmin+eps || x[0]>xmax-eps)*p0 || (x[1]<ymin+eps || x[1]>ymax-eps)*p1 || (x[2]<zmin+eps || x[2]>zmax-eps)*p2", 
+            pmk = Expression("ke*((x[0]<xmin+eps || x[0]>xmax-eps)*p0 || (x[1]<ymin+eps || x[1]>ymax-eps)*p1 || (x[2]<zmin+eps || x[2]>zmax-eps)*p2)", 
                              xmin=self.xmin, xmax=self.xmax, ymin=self.ymin, ymax=self.ymax, zmin=self.zmin, zmax=self.zmax, 
-                             eps=1e-10, p0 = self.PeriodicDir[0], p1 = self.PeriodicDir[1], p2 = self.PeriodicDir[2], domain=self.mymesh, degree=1);
+                             eps=1e-10, p0 = self.PeriodicDir[0], p1 = self.PeriodicDir[1], p2 = self.PeriodicDir[2], ke=self.kappa_e_scalar, domain=self.mymesh, degree=1);
         return pmk
     def ImposeDiffusionTensor(self, k00, k01, k02, k10, k11, k12, k20, k21, k22):
         print("Impose Diffusion Tensor")
