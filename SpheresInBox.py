08f4df5b5be80352134c910ca847bc0303e14452
Van-Dang Nguyen <vdnguyen@kth.se>
2019-07-11 19:17:53 +0200

    Add files via upload

--- SpheresInBox.py
+++ SpheresInBox.py
@@ -9,35 +9,52 @@ import NETGENPlugin
 import inspect, os
 import libSMESH_Swig
 
-# MultiProjection function : project a set of d-1 mesh on another set of faces
+
+# Copyright Dang Van NGUYEN 2015
+
+from MEDLoader import *
+from MEDCoupling import *
+import os, inspect
+
+# MultiProjection function : project a set of d-1 mesh on another set of 
+# faces
 # input : 
 #    mesh :        the global mesh
 #    source :      a set of faces which lean on a same plane
 #    destination : a set of faces which lean on a translated plane 
 #                  (the ref plane is the first one)
 # -----------------------------------
-#def MultiProjection(mesh,source,destination):
-#     ls = geompy.SubShapeAllSortedCentres(source     , geompy.ShapeType["EDGE"])
-#     ld = geompy.SubShapeAllSortedCentres(destination, geompy.ShapeType["EDGE"])
-#     for i in xrange(len(ls)):
-#         a = mesh.Projection1D(ld[i])
-#         a.SourceEdge(ls[i])
-
-def MultiProjection(probdim,mesh, source,destination):
-     if (probdim==2):
+def MultiProjection(probdim,mesh,source,destination,is_cell_cut_bc=False):
+  if (is_cell_cut_bc==True):
+    # this method is better when cells cut the external boundaries
+    if (probdim==2):
+      ls = geompy.SubShapeAllSortedCentres(source     , geompy.ShapeType["EDGE"])
+      ld = geompy.SubShapeAllSortedCentres(destination, geompy.ShapeType["EDGE"])
+      for i in xrange(len(ls)):
+	a = mesh.Projection1D(ld[i])
+	a.SourceEdge(ls[i])
+    if (probdim==3):
+      ls = geompy.SubShapeAllSortedCentres(source     , geompy.ShapeType["FACE"])
+      ld = geompy.SubShapeAllSortedCentres(destination, geompy.ShapeType["FACE"])
+      for i in xrange(len(ls)):
+        a = mesh.Projection2D(ld[i])
+	a.SourceFace(ls[i])
+  else:
+    if (probdim==2):
 	a = mesh.Projection1D(destination)
 	a.SourceEdge(source)
-     if (probdim==3):
+    if (probdim==3):
 	a = mesh.Projection2D(destination)
 	a.SourceFace(source)
 
-# FindFaces function : find in a shape a set of shape leaning on a defined plane
+# FindFaces function : find in a shape a set of shape leaning on a defined
+# plane
 # input : 
 #    shape          : the shape
 #    origin, normal : definition of the ref plane
-#    name           : the name of the set of founded faces
+#    name           : the name of the set of found faces
 # output:
-#    gr : group of the founded faces
+#    gr : group of the found faces
 # -----------------------------------
 def FindFaces(probdim,shape,origin,normal,name):
      if (probdim==2):   
@@ -54,6 +71,137 @@ def FindFaces(probdim,shape,origin,normal,name):
 		geompy.ShapeType["FACE"], normal, origin, geompy.GEOM.ST_ON)
      geompy.UnionList(gr, list)       
      return gr
+
+def unv2xml(directory, unvfilename, is_use_group_name=True,is_regroup=False, markername=[]):
+	# directory=os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))+'/' # script directory
+	unvFile = directory + unvfilename;
+	unvmesh = smesh.CreateMeshesFromUNV(unvFile);
+
+	filewithoutextention = os.path.splitext(os.path.basename(unvFile))[0];
+
+	medFile = directory+filewithoutextention+'.med';
+	unvmesh.ExportMED(medFile)
+
+	# medmesh = unvmesh.GetMEDMesh()
+	medmesh = MEDFileMesh.New(medFile)
+
+	print "Num of vertices: ",medmesh.getNumberOfNodes()
+	dim = medmesh.getMeshDimension()
+	print "Mesh dimension: ",dim
+
+	numvertices = medmesh.getNumberOfNodes();
+	if dim==2:
+	  numcells = unvmesh.NbTriangles()
+	if dim==3:
+	  numcells = unvmesh.NbVolumes()
+	print "Num of cells: ", numcells;
+
+	# Step 2 : Retrieve the lists of Ids of the cells
+	# -----------------------------------
+
+	groupnames=medmesh.getGroupsNames();
+	num_groups = len(groupnames)
+	print 'num groups: ', num_groups
+	if (num_groups>0):
+	  if is_use_group_name == True:
+	     markerarray = [0]*numcells
+	  else:
+	     markerarray = [num_groups]*numcells
+	     # markername = list(xrange(num_groups));
+	  for i in xrange(0,num_groups):
+		groupids = medmesh.getGroupArr(0,groupnames[i]).getValues()
+		if len(groupids)==0:
+		  raise ValueError("Error: Groupe "+ groupnames[i]+" is empty!")
+		for k in xrange(0,len(groupids)):
+			# data = "%d\n" %groupids[k]; filename.write(data);
+			if is_use_group_name == True:
+			  name = int(groupnames[i]);
+			  if is_regroup==True:
+			    name = markername[int(groupnames[i])];
+			  markerarray[groupids[k]] = name;
+			else:
+			  name = markername[i]
+			  markerarray[groupids[k]] = name; 
+			if k==0:
+			  print 'Group', groupnames[i],' is marked with marker', name
+
+	  filename = open(directory+'theta_'+filewithoutextention+'.xml','w');
+	  filename.write('<?xml version="1.0"?>\n')
+	  filename.write('<dolfin xmlns:dolfin="http://fenicsproject.org">\n')
+	  filename.write('  <mesh_function>\n')
+	  filename.write('    <mesh_value_collection type="uint" dim="'+str(dim)+'" size="'+str(numcells)+'">\n');
+	  for i in xrange(0,len(markerarray)):
+	    filename.write('      <value cell_index="'+str(i)+'" local_entity="0" value="'+str(markerarray[i])+'" />\n');
+	  filename.write('    </mesh_value_collection>\n');
+	  filename.write('  </mesh_function>\n');
+	  filename.write('</dolfin>\n');
+	  filename.close();
+
+	os.system('rm '+medFile);
+
+	# export .xml mesh
+	filename = open(directory+filewithoutextention+'.xml','w');
+	filename.write('<?xml version="1.0"?>\n')
+	filename.write('<dolfin xmlns:dolfin="http://fenicsproject.org">\n')
+	if dim==2:
+		filename.write('  <mesh celltype="triangle" dim="2">\n');
+	if dim==3:
+		filename.write('  <mesh celltype="tetrahedron" dim="3">\n');
+	# save mesh vertices
+	filename.write('   <vertices size="'+str(numvertices)+'">\n');
+
+	for node in xrange(1,numvertices+1):
+	  xyz = unvmesh.GetNodeXYZ( node );
+	  if (dim==2):		
+	   	filename.write('	<vertex index="'+str(node-1)+'" x="%.16e" y="%.16e" />\n' % (xyz[0],xyz[1]));	
+	  if (dim==3):		
+	   	filename.write('	<vertex index="'+str(node-1)+'" x="%.16e" y="%.16e" z="%.16e" />\n' % (xyz[0],xyz[1],xyz[2]));	
+	filename.write('   </vertices>\n');
+
+	# save mesh cells
+	filename.write('   <cells size="'+str(numcells)+'">\n');
+	if dim==2:
+		vol = unvmesh.GetElementsByType(SMESH.FACE) 
+	if dim==3:
+		vol = unvmesh.GetElementsByType(SMESH.VOLUME) 
+	# print vol
+	cellindex = 0;
+	for ind in vol:
+	  nodesid = unvmesh.GetElemNodes(ind)
+	  if (dim==2):
+		filename.write('	<triangle index="%d" v0="%d" v1="%d" v2="%d" />\n' % (cellindex,nodesid[0]-1,nodesid[1]-1,nodesid[2]-1) );
+	  if (dim==3):
+		filename.write('	<tetrahedron index="'+str(cellindex)+'" v0="'+str(nodesid[0]-1)+'" v1="'+str(nodesid[2]-1)+'" v2="'+str(nodesid[1]-1)+'" v3="'+str(nodesid[3]-1)+'" />\n');
+	  cellindex = cellindex + 1;
+
+	filename.write('   </cells>\n');	
+
+	filename.write('  </mesh>\n');
+	filename.write('</dolfin>\n');
+	filename.close();
+	if (num_groups>0):
+	  print "Program is terminal normally. Two files were created:";
+	  print 'unvmesh:   '+ directory+filewithoutextention+'.xml';
+	  print 'makerfile: '+directory+'theta_'+filewithoutextention+'.xml';
+	else:
+	  print "Program is terminal normally. One file was created:";
+	  print 'unvmesh:   '+ directory+filewithoutextention+'.xml';
+
+
+def checkProjection(gr, mesh_translated, tol=1e-7):
+    name = gr.GetName() + "_" + mesh_translated.GetName().split("_")[0]
+    mesh_source = smesh.CopyMesh(gr, gr.GetName())
+    mesh_check = smesh.Concatenate([mesh_source.GetMesh(), mesh_translated.GetMesh()], 0)
+    mesh_check.SetName(name)
+    ll_coincident_nodes = mesh_check.FindCoincidentNodes(tol)
+    coincident_nodes = [item for sublist in ll_coincident_nodes for item in sublist]
+    mesh_check.MakeGroupByIds("coincident_nodes", smesh.NODE, coincident_nodes)
+    mesh_nodes = mesh_check.GetNodesId()
+    if len(ll_coincident_nodes) != mesh_translated.NbNodes():
+        print "Projection failed for %s"%name
+        non_coincident_nodes = list(set(mesh_nodes) - set(coincident_nodes))
+        mesh_check.MakeGroupByIds("non_coincident_nodes", smesh.NODE, non_coincident_nodes)
+
 
 directory=os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))+'/' # script directory
 xstart = -5;
@@ -130,15 +278,31 @@ algo1d.LocalLength(0.5)
 algo2d = FinalMesh.Triangle(smesh.NETGEN_2D)
 
 
-MultiProjection(3,FinalMesh, le, ri)
-MultiProjection(3,FinalMesh, fr, be)
-MultiProjection(3,FinalMesh, bo, to)
+MultiProjection(3,FinalMesh, le, ri, True)
+MultiProjection(3,FinalMesh, fr, be, True)
+MultiProjection(3,FinalMesh, bo, to, True)
 
 FinalMesh.Compute()
 
 tol = 1e-4;
 GroupsOfNodes = FinalMesh.FindCoincidentNodes(tol);
 FinalMesh.MergeNodes(GroupsOfNodes);
+
+# Test the peridic boundaries 
+gr_le = FinalMesh.Group(le) 
+gr_ri = FinalMesh.Group(ri) 
+gr_be = FinalMesh.Group(be) 
+gr_fr = FinalMesh.Group(fr) 
+gr_bo = FinalMesh.Group(bo) 
+gr_to = FinalMesh.Group(to)
+
+le_translated = FinalMesh.TranslateObjectMakeMesh( gr_le, smesh.DirStruct(smesh.PointStruct ( 0, yend -ystart , 0 )), 0, 'le_translated')
+be_translated = FinalMesh.TranslateObjectMakeMesh( gr_be, smesh.DirStruct(smesh.PointStruct ( -xstart+xend, 0, 0 )), 0, 'be_translated')
+bo_translated = FinalMesh.TranslateObjectMakeMesh( gr_bo, smesh.DirStruct(smesh.PointStruct ( 0, 0, -zend+zstart )), 0, 'bo_translated')
+
+checkProjection(gr_ri, le_translated) 
+checkProjection(gr_fr, be_translated) 
+checkProjection(gr_to, bo_translated) 
 
 import SMESH
 GroupMesh=FinalMesh.GroupOnGeom(Gp1,"1",SMESH.VOLUME)
